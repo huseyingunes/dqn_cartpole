c7659d7ef62a43315e8c14188a276e85118798cf
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2020-05-12 11:39:30 +0300

    yazım hataları düzeltildi

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ for e in range(episodes):
     #initialize environment
     state = env.reset()
     state = np.reshape(state, [1, 4])
-    time = 0 # zaman en kadar çok geçerse o kadar balarılı çünkü her zamanda 1 ödül alacak
+    time = 0 # zaman ne kadar çok geçerse o kadar balarılı çünkü her zamanda 1 ödül alacak
     while True:
         #act
         action = agent.act(state) # eylemi seç
