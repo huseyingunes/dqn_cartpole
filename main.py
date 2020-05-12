ff295b249da2915e32d5952cd867cf6405f3473d
huseyingunes <huseyingunes@gmail.com>
2020-05-12 03:02:28 +0300

    ilk calisma bitti

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class DQLAgent:
 
     def remember(self, state, action, reward, next_state, done):
         # hafıza -> bilgileri burada depolayacağız
-        self.memory.append(state, action, reward, next_state, done)
+        self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
         # eylem
@@ -78,40 +78,57 @@ class DQLAgent:
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-if __name__ == "main":
+env = gym.make("CartPole-v0")
+agent = DQLAgent(env) # ortam için ajanı üret ve ilk pozisyonunu al
 
-    env = gym.make("CartPole-v0")
-    agent = DQLAgent(env) # ortam için ajanı üret ve ilk pozisyonunu al
+batch_size = 16
+episodes = 50
+for e in range(episodes):
+    #initialize environment
+    state = env.reset()
+    state = np.reshape(state, [1, 4])
+    time = 0 # zaman en kadar çok geçerse o kadar balarılı çünkü her zamanda 1 ödül alacak
+    while True:
+        #act
+        action = agent.act(state) # eylemi seç
 
-    batch_size = 16
-    episodes = 100
-    for e in range(episodes):
-        #initialize environment
-        state = env.reset()
-        state = np.reshape(state, [1, 4])
-        time = 0 # zaman en kadar çok geçerse o kadar balarılı çünkü her zamanda 1 ödül alacak
-        while True:
-            #act
-            action = agent.act(state) # eylemi seç
+        #step
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, 4])
 
-            #step
-            next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state, [1, 4])
+        #remember
+        agent.remember(state, action, reward, next_state, done)
 
-            #remember
-            agent.remember(state, action, reward, next_state, done)
+        #update state
+        state = next_state
 
-            #update state
-            state = next_state
+        #replay
+        agent.replay(batch_size) # rastgele batch_size ı kullanacak
 
-            #replay
-            agent.replay(batch_size) # rastgele batch_size ı kullanacak
+        #adjust epsilon
+        agent.adaptiveEGreedy()
 
-            #adjust epsilon
-            agent.adaptiveEGreedy()
+        time += 1
 
-            time += 1
+        if done:
+            print("Episode: {}, time: {}".format(e, time)) # 200 olursa %100 başarıdır
+            break
 
-            if done:
-                print("Episode: {}, time: {}".format(e, time))
-                break
+# %%
+import time
+trained_model = agent
+state = env.reset()
+state = np.reshape(state, [1, 4])
+time_t = 0
+while True:
+    env.render()
+    action = trained_model.act(state)
+    next_state, reward, done, _ = env.step(action)
+    next_state = np.reshape(next_state, [1, 4])
+    state = next_state
+    time_t += 1
+    print("Zaman :", time_t)
+    time.sleep(.1)
+    if done:
+        break
+print("Bitti")
